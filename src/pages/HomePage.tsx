159e5bcdef05db96e25d225b43a8360316087de7
Caelin Sutch <caelinsutch@gmail.com>
2020-11-22 13:05:26 -0800

    Fixed margin top on button

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -77,6 +77,7 @@ const HomePage: React.FC = () => {
                           Sign up with
                         </Text>
                         <Button
+                          mt={2}
                           leftIcon={<GoogleColorIcon />}
                           onClick={() => setSignupWidgetState(true)}
                         >
